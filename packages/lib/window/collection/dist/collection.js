f836d21cedc0763a17721d5c14111c2cba662304
Shin-Ogata <Shin.Ogata@jp.sony.com>
2020-01-24 19:58:27 +0900

    chore(collection): add collection project
    - `@cdp/collection` プロジェクトの追加
      - 空実装

---
+++ packages/lib/window/collection/dist/collection.js
@@ -0,0 +1,20 @@
+/*!
+ * @cdp/collection 0.9.0
+ *   generic collection scheme
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = global || self, factory(global.CDP = global.CDP || {}));
+}(this, (function (exports) { 'use strict';
+
+	// TODO:
+	const STATUS = 'TODO';
+
+	exports.STATUS = STATUS;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29sbGVjdGlvbi5qcyIsInNvdXJjZXMiOlsiaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gVE9ETzpcbmV4cG9ydCBjb25zdCBTVEFUVVMgPSAnVE9ETyc7XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Q0FBQTtPQUNhLE1BQU0sR0FBRzs7Ozs7Ozs7Ozs7OyIsInNvdXJjZVJvb3QiOiJjZHA6Ly8vQGNkcC9jb2xsZWN0aW9uLyJ9
---
+++ packages/lib/window/collection/tests/unit/collection.spec.ts
@@ -0,0 +1,13 @@
+/*
+ eslint-disable
+   @typescript-eslint/no-explicit-any
+ */
+
+import { STATUS } from '@cdp/collection';
+
+describe('collection spec', () => {
+
+    it('check template', (): void => {
+        expect(STATUS).toBe('TODO');
+    });
+});
