390998f83268335b13b14638a0506c1f8e5684a4
Shin-Ogata <Shin.Ogata@sony.com>
2020-11-30 12:12:15 +0900

    chore(view): update comments
    - `@cdp/view`
      - TODO: コメント修正

--- packages/lib/window/view/types/base.d.ts
+++ packages/lib/window/view/types/base.d.ts
@@ -7,7 +7,46 @@ export declare type ViewFindSelector = Node | string | Nil;
  * @en Base class definition for view that manages the layout and a DOM events.
  * @ja レイアウト管理と DOM イベントの監視を行う View の基底クラス定義
  *
- * TODO:
+ * @example <br>
+ *
+ * ```ts
+ * import { TemplateEngine } from '@cdp/core-template';
+ * import { DOM, dom as $ } from '@cdp/dom';
+ * import { View, ViewEventsHash } from '@cdp/view';
+ * import { ToDo, ToDoEventSource } from './todo';
+ *
+ * const _template = TemplateEngine.compile($('#item-template').html());
+ *
+ * export class ToDoView extends View {
+ *     private _model: ToDo;
+ *     private _$input?: DOM<HTMLInputElement>;
+ *
+ *     constructor(todo: ToDo) {
+ *         super({ tagName: 'li' });
+ *         this._model = todo;
+ *         this.listenTo(this._model as ToDoEventSource, '@change', this.render.bind(this));
+ *         this.listenTo(this._model as ToDoEventSource, '@destroy', this.remove.bind(this));
+ *     }
+ *
+ *     protected events(): ViewEventsHash<HTMLElement> {
+ *         return {
+ *             'click .toggle':   this.toggleDone,
+ *             'dblclick .view':  this.edit,
+ *             'click a.destroy': this.clear,
+ *             'keypress .edit':  this.updateOnEnter,
+ *             'blur .edit':      this.close,
+ *         };
+ *     }
+ *
+ *     render(): this {
+ *         this.$el.html(_template(this._model.toJSON()));
+ *         this.$el.toggleClass('done', this._model.done);
+ *         this._$input = this.$('.edit') as DOM<HTMLInputElement>;
+ *         return this;
+ *     }
+ *     :
+ * }
+ * ```
  */
 export declare abstract class View<TElement extends Node = HTMLElement, TEvent extends object = object> extends EventSource<TEvent> {
     /**
