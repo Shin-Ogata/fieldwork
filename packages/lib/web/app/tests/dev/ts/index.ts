d9554cf7900e27cebecaec9d93a8feeabf73c871
Shin Ogata <Shin.Ogata@sony.com>
2023-01-10 11:16:22 +0900

    chore: update sdks
    - i18next: v22.4.9
    - lit-html: v2.6.0
    - rollup: v3.9.1
    - sass: v1.57.1
    - testem: v3.10.1
    - @rollup/plugin-commonjs: v23.0.5
    
    - chore(web/app): memo

--- packages/lib/web/app/tests/dev/ts/index.ts
+++ packages/lib/web/app/tests/dev/ts/index.ts
@@ -1,5 +1,15 @@
-import { getConfig } from '@cdp/core-utils';
+import { AppContext } from '@cdp/app';
 import './page';
 
+// TODO:
+import { getConfig } from '@cdp/core-utils';
 const config = getConfig();
 console.log(config);
+
+void (async () => {
+
+    await AppContext({
+        initialPath: '/root',
+    }).ready;
+
+})();
--- packages/lib/web/app/tests/dev/ts/page/page-root/context.ts
+++ packages/lib/web/app/tests/dev/ts/page/page-root/context.ts
@@ -1,6 +1,20 @@
 import { getConfig } from '@cdp/core-utils';
+import type { RouteChangeInfo } from '@cdp/router';
+import { registerPage } from '@cdp/app';
 
 const config = getConfig();
 
-export const PAGE_CONTEXT_ROOT = 'PAGE_CONTEXT_ROOT';
+const PAGE_CONTEXT_ROOT = 'PAGE_CONTEXT_ROOT';
 config['PAGE_CONTEXT_ROOT'] = PAGE_CONTEXT_ROOT;
+
+// TODO: content の template の扱いを思い出す
+registerPage({
+    path: '/root',
+    component: {
+        name: 'I was born from an object.',
+        pageInit(info: RouteChangeInfo) {
+            console.log(info.to.path);
+        }
+    },
+    content: '#page-root',
+});
